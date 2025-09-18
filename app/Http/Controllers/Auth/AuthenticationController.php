c34ceb6f6f60b58451c732eefde6f8436546836f
ernestwijaya <ernestwijaya_2428250010@mhs.mdp.ac.id>
2025-09-18 18:41:45 +0700

    Push 2

--- app/Http/Controllers/Auth/AuthenticationController.php
+++ app/Http/Controllers/Auth/AuthenticationController.php
@@ -5,16 +5,33 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Models\User;
 use Exception;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Support\Facades\Auth as FacadesAuth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthenticationController extends Controller
 {
     public function login(LoginRequest $request)
     {
         try {
+            $validated = $request -> safe()->all();
+            if (!Auth::attempt($validated)){
+                return response()->json([
+                    'message' => "Email Atau Password Salah",
+                    'date' => null
+                ], 401);
+            }
 
+            $user = $request-> user();
+            $token = $user->create_token('laravel_api', ['*'])-> plainTextToken;
+            
         } catch (Exception $e) {
-            //
+            return response()->json([
+                    'message' => $e ->getMessage(),
+                    'date' => null
+                ], 201);
         }
     }
 
@@ -23,7 +40,7 @@ class AuthenticationController extends Controller
         try {
             $validated = $request->safe()->all();
             $passwordhash = Hash::make($validated['password']);
-            $validated['password'] = $psswordhash;
+            $validated['password'] = $passwordhash;
             $response = User::create($validated);
 
             if($response) {
@@ -33,7 +50,10 @@ class AuthenticationController extends Controller
                 ], 201);
             }
         } catch (Exception $e) {
-            //
+            return response()->json([
+                    'message' => $e ->getMessage(),
+                    'date' => null
+                ], 201);
         }
     }
 
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::middleware('guest')-> group(function(){
+    Route::post('/register', [AuthenticationController::class, 'register']);
+    Route::post('/login', [AuthenticationController::class, 'login']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
